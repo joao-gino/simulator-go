731190e4b53f5f15be0bdb31d3a06568633f1f6e
João Vitor Gino <joaovitorsrg@hotmail.com>
2022-06-24 11:05:38 -0300

    inital commit

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/codeedu/imersaofsfc2-simulator/application/route"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+}
+
+func main() {
+	route := route.Route{
+		ID:       "1",
+		ClientID: "1",
+	}
+
+	route.LoadPositions()
+	stringjson, _ := route.ExportJsonPositions()
+	fmt.Println(stringjson[0])
+}
